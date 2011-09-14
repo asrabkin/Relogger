e2732e732e5ed91a2a84cb933d6770cddff87287
asrabkin <asrabkin@cloudera.com>
2011-09-13 23:18:05 -0700

    refactor and use two-level caching of on bits

--- src/edu/berkeley/LogPerfTest.java
+++ src/edu/berkeley/LogPerfTest.java
@@ -1,5 +1,6 @@
 package edu.berkeley;
 
+import java.util.BitSet;
 import org.apache.commons.logging.Log;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -7,7 +8,7 @@ import org.apache.log4j.Logger;
 
 public class LogPerfTest {
 
-  public static final long RUNS = 10* 1000*1000;
+  public static final long RUNS = 100* 1000*1000;
   static Logger LOG = Logger.getLogger(LogPerfTest.class);
   static org.apache.commons.logging.Log ApacheLOG =
     org.apache.commons.logging.LogFactory.getLog(LogPerfTest.class);
@@ -17,17 +18,51 @@ public class LogPerfTest {
   
   public static void main(String[] args) {
     LOG.setLevel(Level.INFO);
+    
+    if(args.length > 0) {
+      testBitsetCopy();
+    }
 
-    for(int i=0; i < RUNS; ++i)
-      LOG.trace("I am a warmup log statement");
-
+    for(int i=0; i < RUNS; ++i) {
+      LOG.isTraceEnabled();
+      printATrace("I am a warmup log statement");
+    }
+    
+    
     long startT = System.currentTimeMillis();
     for(int i=0; i < RUNS; ++i)
-      LOG.trace("I am a simple log statement");
+      printATrace("I am a simple log statement");
     long duration = System.currentTimeMillis() - startT;
     long ns_per_log = duration * 1000 * 1000 / RUNS;
     System.out.println(ns_per_log + "ns per un-used log stmt");
     
+  }
+  private static void printATrace(String s) {
+    LOG.trace(s);
+  }
+  private static void testBitsetCopy() {
+    int BITSET_RUNS = 1000000;
+
+    BitSet b = new BitSet(6000);
+    for(int i=0; i < BITSET_RUNS; ++i)
+      doCopy(b);
+
+    long startT = System.currentTimeMillis();
+    for(int i=0; i < BITSET_RUNS; ++i)
+      doCopy(b);
+    long duration = System.currentTimeMillis() - startT;
+    long ns_per_log = duration * 1000 * 1000 / BITSET_RUNS;
+    System.out.println(ns_per_log + "ns per bitset copy; bitset size is " + b.size());
+  
+  }
+  private static BitSet doCopy(BitSet b) {
+    BitSet newTable;
+    synchronized(b) {
+      int newLen = b.size();
+      newTable = new BitSet(newLen);
+      newTable.or(b);
+    }
+    return newTable;
   }
 
 }
--- src/edu/berkeley/numberlogs/NumberedLogging.java
+++ src/edu/berkeley/numberlogs/NumberedLogging.java
@@ -5,35 +5,81 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.apache.log4j.*;
 
 public class NumberedLogging {
-  
-  static volatile BitSet globalMaskTable = new BitSet();
-  static ConcurrentHashMap<Integer, String> warnLevels = new ConcurrentHashMap<Integer, String>();
-  
-  
-  public static Level levNameToLevel(String s) {
-    return Level.toLevel(s);
-  }
-  
-  
-  static boolean isDisabled(int globalID) {
-    return globalMaskTable.get(globalID);
-  }
-  
-  static synchronized void update(int i, boolean isDisabled) {
-    int newLen = globalMaskTable.length();
-    if (i > newLen)
-        newLen = i;
-    BitSet newTable = new BitSet(newLen);
-    newTable.set(i, isDisabled);
-    globalMaskTable = newTable;
-  }
-  
 
-  public static String setMeth(int line, String methname) {
-    warnLevels.put(line, methname);
-    return methname;
+  static volatile BitSet cachedMaskTable = new BitSet();
+  static BitSet userMaskTable = new BitSet();
+
+  static ConcurrentHashMap<Integer, LEVS> warnLevels = new ConcurrentHashMap<Integer, LEVS>();
+
+
+  public enum LEVS {FATAL, ERROR, WARN, INFO, DEBUG,TRACE, UNKNOWN};
+
+  public static LEVS levNameToLevel(String methname) {
+    if(methname.equals("fatal"))
+      return LEVS.FATAL;
+    else if(methname.equals("error"))
+      return LEVS.ERROR;
+    else if(methname.equals("warn"))
+      return LEVS.WARN;
+    else if(methname.equals("info"))
+      return LEVS.INFO;
+    else if(methname.equals("debug"))
+      return LEVS.DEBUG;
+    else if(methname.equals("trace"))
+      return LEVS.TRACE;
+    else
+      return LEVS.UNKNOWN;
   }
-  
+
+
+  static boolean isDisabled(int globalID) {
+    return cachedMaskTable.get(globalID);
+  }
+
+  static synchronized void update(int i, boolean isDisabled) {
+    //Updates both user and cached map table
+
+    userMaskTable.set(i, isDisabled);
+
+    int newLen = cachedMaskTable.size();
+    if (i > newLen)
+      newLen = i;
+
+    BitSet newTable = new BitSet(newLen);
+    newTable.or(cachedMaskTable);
+    newTable.set(i, isDisabled);
+    cachedMaskTable = newTable;
+  }
+
+
+  public static synchronized void cached_disable(int i) {
+    //    System.out.println("cache-disable " + i);
+    int newLen = cachedMaskTable.size();
+    if (i > newLen)
+      newLen = i;
+    BitSet newTable = new BitSet(newLen);
+    newTable.or(cachedMaskTable);
+    newTable.set(i);
+    cachedMaskTable = newTable;
+  }
+
+  public static LEVS setMeth(int line, String methname) {
+    LEVS lev = levNameToLevel(methname);
+    warnLevels.put(line, lev);
+    return lev;
+  }
+
+
+  private static LEVS getWarnLevel(int id) {
+    LEVS l = warnLevels.get(id);
+    /*
+   if(l == null)
+      return LEVS.UNKNOWN;
+    else*/
+    return l;
+    //    return LEVS.TRACE;
+  }
+
   public static String reformatArray(Object[] arr) {
 
     StringBuilder sb = new StringBuilder();
@@ -50,64 +96,101 @@ public class NumberedLogging {
     sb.append("}");
     return sb.toString();
   }
-  
-  public static void logmsg(int id, String original_methname, Logger log, Object[] args) {
-    
-    //    if(!globalMaskTable.get(id)) //NOTE this is backwards for testing.
-    if(globalMaskTable.get(id))
-      return;
 
-    String methname = original_methname;
-    //    String methname = warnLevels.get(id);
+  public static void logmsg(int id, String original_methname, Logger log, Object[] args) {
+
+    //    if(!cachedMaskTable.get(id)) //NOTE this is backwards for testing.
+    if(cachedMaskTable.get(id))
+      return;
+    //   System.out.println("cache-miss, doing long resolve");
+
+    LEVS methname = getWarnLevel(id);
     if(methname == null)
       methname = setMeth(id, original_methname);
 
-    if(methname == "fatal") {
-      if(args.length == 1) 
-        log.fatal("(" + id + ") " +args[0]);
+    switch(methname) {
+    case FATAL:
+      log4J_fatal(log, id, args);
+      break;
+    case ERROR:
+      log4J_error(log, id, args);
+      break;
+    case WARN:
+      log4J_warn(log, id, args);
+      break;
+    case INFO:
+      if(log.isInfoEnabled())
+        log4J_info(log, id, args);
       else
-        log.fatal("(" + id + ") " +args[0], (Throwable) args[1]);
-    } else if(methname == "error") {
-      if(args.length == 1) 
-        log.error("(" + id + ") " +args[0]);
-      else
-        log.error("(" + id + ") " +args[0], (Throwable) args[1]);
-    } else if(methname == "warn") {
-      if(args.length == 1) 
-        log.warn("(" + id + ") " +args[0]);
-      else
-        log.warn("(" + id + ") " +args[0], (Throwable) args[1]);
-   }else if(methname ==  "info") {
-     if(args.length == 1) 
-       log.info("(" + id + ") " +args[0]);
-     else
-       log.info("(" + id + ") " +args[0], (Throwable) args[1]);
-  } else if(methname == "debug") {
+        cached_disable(id);
+      break;
+    case DEBUG:
       if(log.isDebugEnabled())
-        if(args.length == 1) 
-          log.debug("(" + id + ") " +args[0]);
-        else
-          log.debug("(" + id + ") " +args[0], (Throwable) args[1]);
-   } else if(methname == "trace") {
-       if(log.isTraceEnabled())
-         if(args.length == 1) 
-           log.trace("(" + id + ") " +args[0]);
-         else
-           log.trace("(" + id + ") " +args[0], (Throwable) args[1]);
-  } else {
-     log.info(methname + "(" + id  + ") " +args[0]);
-   }
+        log4J_debug(log, id, args);
+      else
+        cached_disable(id);
+      break;
+    case TRACE: 
+      if(log.isTraceEnabled())
+        log4J_trace(log, id, args);
+      else
+        cached_disable(id); 
+      break;
+    default:
+      //Or should do something dramatic here?
+          log.info(methname + "(" + id  + ") " +args[0]);
+    }
   }
 
+  public static void log4J_fatal(Logger log, int id, Object[] args) {
+    if(args.length == 1) 
+      log.fatal("(" + id + ") " +args[0]);
+    else
+      log.fatal("(" + id + ") " +args[0], (Throwable) args[1]);
+  }
+  public static void log4J_error(Logger log, int id, Object[] args) {
+    if(args.length == 1) 
+      log.error("(" + id + ") " +args[0]);
+    else
+      log.error("(" + id + ") " +args[0], (Throwable) args[1]);
+  }
+  public static void log4J_warn(Logger log, int id, Object[] args) {
+    if(args.length == 1) 
+      log.warn("(" + id + ") " +args[0]);
+    else
+      log.warn("(" + id + ") " +args[0], (Throwable) args[1]);
+  }
+  public static void log4J_info(Logger log, int id, Object[] args) {
+    if(args.length == 1) 
+      log.info("(" + id + ") " +args[0]);
+    else
+      log.info("(" + id + ") " +args[0], (Throwable) args[1]);
+  }
+  public static void log4J_debug(Logger log, int id, Object[] args) {
+    if(args.length == 1) 
+      log.debug("(" + id + ") " +args[0]);
+    else
+      log.debug("(" + id + ") " +args[0], (Throwable) args[1]);
+  }
+  public static void log4J_trace(Logger log, int id, Object[] args) {
+    System.out.println("Trace is enabled");
+    System.exit(0);
+    if(args.length == 1) 
+      log.trace("(" + id + ") " +args[0]);
+    else
+      log.trace("(" + id + ") " +args[0], (Throwable) args[1]);
+  }
+
+
   public static void logmsg(int id, String original_methname, org.apache.commons.logging.Log log, Object[] args) {
-    
+
     if(isDisabled(id))
       return;
-    
-    String methname = warnLevels.get(id);
+
+    LEVS methname = warnLevels.get(id);
     if(methname == null)
-      methname = original_methname;
-    
+      methname = setMeth(id, original_methname);
+
     if(methname.equals("fatal")) {
       if(args.length == 1) 
         log.fatal("(" + id + ") " +args[0]);
@@ -123,27 +206,27 @@ public class NumberedLogging {
         log.warn("(" + id + ") " +args[0]);
       else
         log.warn("(" + id + ") " +args[0], (Throwable) args[1]);
-   } else if(methname.equals("info")) {
-     if(args.length == 1) 
-       log.info("(" + id + ") " +args[0]);
-     else
-       log.info("(" + id + ") " +args[0], (Throwable) args[1]);
-   } else if(methname.equals("debug")) {
+    } else if(methname.equals("info")) {
+      if(args.length == 1) 
+        log.info("(" + id + ") " +args[0]);
+      else
+        log.info("(" + id + ") " +args[0], (Throwable) args[1]);
+    } else if(methname.equals("debug")) {
       if(args.length == 1) 
         log.debug("(" + id + ") " +args[0]);
       else
         log.debug("(" + id + ") " +args[0], (Throwable) args[1]);
-   } else if(methname.equals("trace")) {
-     if(log.isTraceEnabled())
-       if(args.length == 1) 
-         log.trace("(" + id + ") " +args[0]);
-       else
-         log.trace("(" + id + ") " +args[0], (Throwable) args[1]);
-   } else {
-     log.info(methname + "(" + id  + ") " +args[0]);
-   }
-    
-//    System.out.println(reformatArray(args));
+    } else if(methname.equals("trace")) {
+      if(log.isTraceEnabled())
+        if(args.length == 1) 
+          log.trace("(" + id + ") " +args[0]);
+        else
+          log.trace("(" + id + ") " +args[0], (Throwable) args[1]);
+    } else {
+      log.info(methname + "(" + id  + ") " +args[0]);
+    }
+
+    //    System.out.println(reformatArray(args));
   }
-  
+
 }
